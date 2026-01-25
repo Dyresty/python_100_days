6e463cd079809cadf46abe0b41b12705bf9b9b36
Rochan <srochankumar.srk@gmail.com>
2026-01-25 10:45:19 +0100

    tkinter updates

--- day 27/Arguments 27.py
+++ day 27/Arguments 27.py
@@ -1,5 +1,6 @@
 #*args is used to pass a variable number of arguments to a function.
 #can use * and anm name. like *nums or *vals
+# args returns a tuple
 def add(*args):
 
     print(args[1])
@@ -13,3 +14,30 @@ print(add(3, 4, 5, 6, 7, 8, 9))
 
 
 # **kwargs allows you to pass keyworded variable-length arguments to a function.
+# kwargs returns a dictionary
+def calculate(n, **kwargs):
+    print(kwargs)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+calculate(2, add=3, multiply=5)
+
+
+# Using **kwargs in a class
+class Car:
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+        self.year = kw.get("year")
+        self.color = kw.get("color")
+
+my_car = Car(make="Nissan", model="GT-R", year=2020, color="White")
+print(my_car.model)
+
+
+# Combining *args and **kwargs
+def all_aboard(a, *args, **kw): 
+    print(a, args, kw)
+
+all_aboard(4, 7, 3, 0, x=10, y=64)
--- day 27/tkinter intro 27.py
+++ day 27/tkinter intro 27.py
@@ -4,10 +4,64 @@ window.title("GUI Program")
 window.minsize(width=600, height=450)
 
 mylabel = tk.Label(text="This is a label", font=("Arial", 24, "bold"))
+#mylabel["text"] = "New Text"
+#mylabel.config(text="New Text")
 mylabel.pack()
 
+mylabel.config(text="New Text")
+
+
+
+# Button
+def button_clicked():
+    mylabel.config(text=input.get())
+
+button = tk.Button(text="Click Me", command=button_clicked)
+button.pack()
+
+
+
+# Entry
+input = tk.Entry(width=30)
+input.pack()
+print(input.get())
+
+
+
+# Text    
+text = tk.Text(height=5, width=30)
+#focus puts cursor into text box
+text.focus()
+text.insert(tk.END, "Example of multi-line text entry.")
+print(text.get("1.0", tk.END))
+text.pack()
+
+
+
+# Spinbox
+def spinbox_used():
+    print(spinbox.get())
+spinbox = tk.Spinbox(from_=0, to=10, width=5, command=spinbox_used)
+spinbox.pack()
+
+
+
+# Scale
+def scale_used(value):
+    print(value)
+scale = tk.Scale(from_=0, to=100, command=scale_used)
+scale.pack()
+
+# Checkbutton
+def checkbutton_used():
+    print(checked_state.get())
+checked_state = tk.IntVar()
+checkbutton = tk.Checkbutton(text="Is On?", variable=checked_state, command=checkbutton_used)
+checkbutton.pack()
+
+
+
+
+
 window.mainloop()
-
-
-
 
