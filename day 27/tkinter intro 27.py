f38bde194bfe36959fd7fba198169e61a2fb553b
Rochan <srochankumar.srk@gmail.com>
2026-01-26 20:38:07 +0100

    day 28 to 30

--- day 27/tkinter intro 27.py
+++ day 27/tkinter intro 27.py
@@ -60,8 +60,25 @@ checkbutton = tk.Checkbutton(text="Is On?", variable=checked_state, command=chec
 checkbutton.pack()
 
 
+def radio_used():
+    print(radio_state.get())  
+radio_state = tk.IntVar()
+radiobutton1 = tk.Radiobutton(text="Option 1", value=1, variable=radio_state, command=radio_used)
+radiobutton2 = tk.Radiobutton(text="Option 2", value=2, variable=radio_state, command=radio_used)
+tk.command=radio_used
+radiobutton1.pack()
+radiobutton2.pack()
 
 
+# Listbox
+listbox = tk.Listbox(height=4)
+fruits = ["Apple", "Banana", "Cherry", "Date"]
+for item in fruits:
+    listbox.insert(fruits.index(item), item)
+def listbox_used(event):
+    print(listbox.get(listbox.curselection()))  
+listbox.bind("<<ListboxSelect>>", listbox_used)
+listbox.pack()
 
 window.mainloop()
 
